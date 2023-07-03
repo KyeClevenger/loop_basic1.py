5061a6df1b70410df192475caa44ad99ac017f25
Kye Clevenger <Kyeclevenger@gmail.com>
2023-07-03 16:59:11 -0600

    first

---
+++ basics.py
@@ -0,0 +1,39 @@
+for i in range (151):
+    print(i)
+    
+
+
+for i in range (5, 1001, 5):
+    print (i)
+
+
+
+print ("Coding Dojo")
+
+elif i % 5 == 0:
+        print ("Coding")
+
+else:
+        print(i)
+
+
+    i = 0
+for add in range (1, 500001, 2):
+    i = i + add
+
+        print(i)
+
+
+
+for i in range (2018, 0, -4):
+        print(i)
+
+
+
+lownum= int(input('num_1:'))
+highnum = int(input('num_2:'))
+mult = int(input('num_3:'))
+
+for i in range (lownum,highnum +1,1):
+    if i % mult == 0:
+        print(i)
